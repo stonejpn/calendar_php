f5d9bf3867d56549992a776d9a5e7f5244051eb3
Satoshi Tonegawa <stone@328w.co.jp>
2024-05-19 21:48:27 +0900

    feat エラーページを実装

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,5 +4,22 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $path_info = array_key_exists('PATH_INFO', $_SERVER) ? $_SERVER['PATH_INFO'] : '';
 $start_date = array_key_exists('start-date', $_COOKIE) ? $_COOKIE['start-date'] : '';
-$app = new \Calendar\CalendarApp($path_info, $start_date);
-$app->display();
+try {
+    $app = new \Calendar\CalendarApp($path_info, $start_date);
+    $app->display();
+} catch (Exception $e) {
+    print <<<EOD
+<!doctype html>
+<html lang="ja">
+<head>
+  <title>エラー</title>
+</head>
+<body>
+<div style="margin: 2rem; margin-left: 25%; border: 1px #DDDDDD solid; padding 1rem; width: 50%;">
+<div style="padding: 0.3rem 0; background-color: #FFEEEE; font-size: 24px; text-align: center; font-weight: bold">ERROR</div>
+<div style="padding: 1rem;">{$e->getMessage()}</div>
+</div>
+</body>
+</html>
+EOD;
+}
